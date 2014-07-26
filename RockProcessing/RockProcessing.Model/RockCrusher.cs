8b1407490d308fda248cfbd21c2fe819a014dd83
jpa1408 <jpa1408@hotmail.com>
2014-07-26 20:58:04 +0100

    Added code sufficient to pass failing end to end test where total process time for items requiring crushing is evaluated

--- RockProcessing/RockProcessing.Model/RockCrusher.cs
+++ RockProcessing/RockProcessing.Model/RockCrusher.cs
@@ -4,9 +4,13 @@ namespace RockProcessing.Model
 {
 	public class RockCrusher : RockProcessor
 	{
+		public static double ProcessTimePerKilo = 50;
+
 		public override void ProcessRock(RockJob rockJob) {
 			double variableChange = RandomDouble(20.0, 30.0);
+			var preProcessWeight = rockJob.CurrentWeight;
 			rockJob.CurrentWeight = Math.Round(rockJob.CurrentWeight - ((rockJob.CurrentWeight / 100) * variableChange), 2);
+			rockJob.ProcessTime = preProcessWeight * ProcessTimePerKilo;
 		}
 	}
 }
