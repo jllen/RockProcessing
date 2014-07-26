576e1d64129f3aaa29125c2b5588a05e29e345b5
jpa1408 <jpa1408@hotmail.com>
2014-07-26 09:00:01 +0100

    Created base projects for model and test.

---
+++ RockProcessing/RockProcessing.Test/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace RockProcessing.Test{
+	[TestFixture]
+    public class Class1{
+		[Test]
+		public void Test1(){
+			
+		}
+    }
+}
